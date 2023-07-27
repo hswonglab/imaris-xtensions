a019fcea7a57f0e60bc8457be605904fe2f94f62
cs <cs.temporary@icloud.com>
2023-07-27 01:39:21 +0000

    Add ConfigureChannels XTension with README

---
+++ README.md
@@ -0,0 +1,54 @@
+# Wong Lab Imaris XTensions
+
+## Installation
+
+To use these XTensions, follow these instructions:
+
+1. Clone or download this repository to your local machine. You can place it
+   wherever you like.
+2. Open Imaris.
+3. Open the Imaris preferences at "File > Preferences".
+4. Under "Custom Tools" make sure the MATLAB runtimes are present (use the links
+   in Imaris to download any missing runtimes that the XTensions you want to use
+   require) and that you have specified a path to a Python 3.7 executable. If
+   you need to install Python 3.7, you can do so using Anaconda.
+5. Under the locations for Imaris to look for XTensions, add the "python" and/or
+   "matlab" folders of this repository depending on which XTensions you want.
+6. Restart Imaris.
+7. The XTensions in this repository should appear in the "Image Processing" menu
+   once you open an image. Click on an XTension's name to run it.
+
+## How XTensions are Listed in Imaris
+
+In each XTension file, you will see a comment with XML content like this (for
+Python):
+
+```xml
+<CustomTools>
+  <Menu>
+   <Item name="XTension Name" icon="Python3" tooltip="XTension description.">
+     <Command>Python3XT::XTensionName(%i)</Command>
+   </Item>
+  </Menu>
+</CustomTools>
+```
+
+Imaris would display an entry under "Image Processing" for an extension called
+`XTension Name`, and hovering over it would bring up a tooltip with `XTension
+description.`. When you click on that entry, Imaris will look for a function
+named `XTensionName` in a file called `XTension.py` and execute that function.
+
+## Acknowledgements
+
+These XTensions were created in the
+[Wong Lab](https://biology.mit.edu/profile/harikesh-s-wong/), an
+[MIT](https://mit.edu) lab at the
+[Ragon Institute](https://ragoninstitute.org/) of Mass General, MIT, and
+Harvard.
+
+This work was supported by a variety of funding sources, including:
+
+* The National Institute of General Medical Sciences of the National Institutes
+  of Health under award number T32GM87237-14. The content is solely the
+  responsibility of the authors and does not necessarily represent the official
+  views of the National Institutes of Health.
---
+++ python/ConfigureChannels.py
@@ -0,0 +1,147 @@
+#  ConfigureChannels: An Imaris XTension to apply stored settings to channels
+#
+#  Copyright © 2023 MASSACHUSETTS INSTITUTE OF TECHNOLOGY.
+#  All rights reserved.
+#
+#  Written by Christopher Skalnik.
+#
+#    <CustomTools>
+#      <Menu>
+#       <Item name="Configure Channels" icon="Python3" tooltip="Test XTension for debugging.">
+#         <Command>Python3XT::ConfigureChannels(%i)</Command>
+#       </Item>
+#      </Menu>
+#    </CustomTools>
+
+'''ConfigureChannels allows loading channel names and colors from a CSV file.
+
+The CSV should have one row per channel, in the order in which those channels
+appear in Imaris, where each row has the following columns:
+
+* channel: Intended to be the index (1-indexed) of the channel, but ignored by
+  this code.
+* setting: Intended to be the sequential (a.k.a. setting) number in LAS X, but
+  ignored by this code.
+* fluorophore: The fluorophore being detected by the channel, for example
+  "AF555".
+* target: The protein or other cell feature that is supposed to be labeled by
+  the fluorophore.
+* color: The RGB color in which to display the channel, represented as 3
+  hexadecimal bytes (e.g. `ffffff` for white or `ff0000` for red).
+
+An error will be thrown if the first row of the CSV (the header) does not have
+exactly these columns, in this order.
+
+Whenever this XTension operates on a file, it tracks all changes in a log file.
+If the file being modified is at path `path`, then the log file is at
+`path.txt`. Note however that this XTension cannot save changes to the file, so
+logged changes may not actually be saved if the user chooses not to.
+'''
+
+import csv
+import logging
+import traceback
+
+import ImarisLib
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+EXPECTED_HEADER = ['channel', 'setting', 'fluorophore', 'target', 'color']
+LOG_FORMAT = '%(asctime)s %(levelname)s [%(pathname)s:%(lineno)d %(name)s] %(message)s'
+
+def Main(vImarisApplication):
+    image_path = vImarisApplication.GetCurrentFileName()
+    logpath = image_path + '.log'
+    logging.basicConfig(format=LOG_FORMAT, filename=logpath, level=logging.INFO)
+    logging.info('----- Begin Editing %s -----', image_path)
+
+    # Get the image and channels
+    vNumberOfImages = vImarisApplication.GetNumberOfImages()
+    if vNumberOfImages != 1:
+        messagebox.showwarning('Only 1 image may be open at a time for this XTension')
+        return
+
+    vImage = vImarisApplication.GetImage(0)
+    vNumChannels = vImage.GetSizeC()
+    vOldChannelNames = [vImage.GetChannelName(i) for i in range(vNumChannels)]
+    vOldChannelColors = [vImage.GetChannelColorRGBA(i) for i in range(vNumChannels)]
+    vOldChannelColorStrings = ['%08x' % color for color in vOldChannelColors]
+
+    with filedialog.askopenfile(mode='r', title='Select CSV specifying panel') as f:
+        logging.info('Using panel from %s', f.name)
+        reader = csv.reader(f)
+        header = next(reader)
+        if header != EXPECTED_HEADER:
+            raise RuntimeError(
+                f'Panel has unexpected format. Got header {header} instead of {EXPECTED_HEADER}'
+            )
+            return
+        vNewChannelColors = []
+        vNewChannelNames = []
+        for _, _, fluorophore, target, rgb in reader:
+            if len(rgb) != 6:
+                raise RuntimeError(f'Invalid color for {fluorophore} {target}: {rgb}')
+                return
+            vNewChannelNames.append(f'{target} {fluorophore}')
+            # The CSV specifies color as RGB
+            red = rgb[0:2]
+            green = rgb[2:4]
+            blue = rgb[4:6]
+            # The color is represented as ABGR where each of alpha, blue, green, and red are represented by
+            # a byte and A is the most-significant byte. We set an opacity of 0, which indicates no transparency.
+            vNewChannelColors.append(int(f'00{blue}{green}{red}', 16))
+        vNewChannelColorStrings = ['%08x' % color for color in vNewChannelColors]
+
+    confirmed = messagebox.askokcancel(
+        'Confirm Changes',
+        (
+            f'Rename channels {vOldChannelNames} to {vNewChannelNames} '
+            f'and re-color from {vOldChannelColorStrings} to {vNewChannelColorStrings}?'
+        )
+    )
+    if confirmed:
+        print(f'Renaming channels {vOldChannelNames} to {vNewChannelNames}.')
+        print(f'Re-coloring from {vOldChannelColorStrings} to {vOldChannelColorStrings}.')
+    else:
+        print('Changes aborted.')
+        return
+    logging.info('Renaming channels from %s to %s.', vOldChannelNames, vNewChannelNames)
+    vImageNew = vImage.Clone()
+    for i, vNewName in enumerate(vNewChannelNames):
+        vImageNew.SetChannelName(i, vNewName)
+    logging.info('Channel renaming complete.')
+    logging.info('Re-coloring channels from %s to %s', vOldChannelColorStrings, vNewChannelColorStrings)
+    for i, vNewColor in enumerate(vNewChannelColors):
+        vImageNew.SetChannelColorRGBA(i, vNewColor)
+    logging.info('Channel re-coloring complete.')
+    vImarisApplication.SetImage(0, vImageNew)
+    logging.info('Note that user must save image manually, otherwise changes will be lost.')
+    logging.info('----- Done Editing %s -----', image_path)
+    print('Changes complete.')
+
+
+def ConfigureChannels(aImarisId):
+    # Create an ImarisLib object
+    vImarisLib = ImarisLib.ImarisLib()
+
+    # Get an imaris object with id aImarisId
+    vImarisApplication = vImarisLib.GetApplication(aImarisId)
+
+    # Initialize and launch Tk window, then hide it.
+    vRootTkWindow = Tk()
+    vRootTkWindow.withdraw()
+
+    # Check if the object is valid
+    if vImarisApplication is None:
+        messagebox.showerror('Error', f'Failed to connect to Imaris application (id={aImarisId})')
+        return
+
+    print(f'Connected to Imaris application (id={aImarisId})')
+
+    try:
+        Main(vImarisApplication)
+    except Exception as exception:
+        print(traceback.print_exception(type(exception), exception, exception.__traceback__))
+    messagebox.showinfo('Complete', 'The XTension has terminated.')
