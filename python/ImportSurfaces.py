f722a4c732e0607f35c6a55a1c33ea7d01b10989
Eric Gai <gaieric@icloud.com>
2025-11-05 07:27:22 -0500

    adding xtension for importing surfaces from json format

---
+++ python/ImportSurfaces.py
@@ -0,0 +1,118 @@
+#  ImportSurfaces: An Imaris XTension to export surfaces.
+#
+#  Copyright © 2023-2025 MASSACHUSETTS INSTITUTE OF TECHNOLOGY.
+#  All rights reserved.
+#
+#  Written by Amy Huang. Based off of ExportSurfaces.py by Chris Skalnik. 
+#
+#    <CustomTools>
+#      <Menu>
+#       <Item name="Import Surfaces" icon="Python3" tooltip="Import surface objects.">
+#         <Command>Python3XT::ImportSurfaces(%i)</Command>
+#       </Item>
+#      </Menu>
+#    </CustomTools>
+
+'''ImportSurfaces exports the surfaces in an Imaris file for use outside Imaris.
+'''
+
+import csv
+import json
+import logging
+import os
+import sys
+import traceback
+
+import ImarisLib
+import Imaris
+
+from tkinter import Tk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+
+# Some DLLs are stored at this path, but it isn't correctly set by default. We
+# can't just set the system environment variable because doing so adds a space
+# at the end of the path for some reason. This means that instead of searching
+# for \path\to\bin\myDll.dll, it searches for \path\to\bin\ myDll.dll.
+DLL_PATH = os.path.join(os.path.dirname(sys.executable), 'Library', 'bin')
+os.environ['PATH'] += f';{DLL_PATH}'
+
+import numpy as np
+
+LOG_FORMAT = '%(asctime)s %(levelname)s [%(pathname)s:%(lineno)d %(name)s] %(message)s'
+
+def Main(vImarisApplication):
+    image_path = vImarisApplication.GetCurrentFileName()
+    logpath = image_path + '.log'
+    logging.basicConfig(format=LOG_FORMAT, filename=logpath, level=logging.INFO)
+    logging.info('----- Begin importing surfaces to %s -----', image_path)
+
+    # Get the image and channels
+    vNumberOfImages = vImarisApplication.GetNumberOfImages()
+    if vNumberOfImages != 1:
+        messagebox.showwarning('Only 1 image may be open at a time for this XTension')
+        return
+
+    vSurfaces = vImarisApplication.GetFactory().CreateSurfaces()
+    
+    logging.info('Asking user to select json')
+    with filedialog.askopenfile(mode='r', title='Select json representing Imaris surfaces') as f:
+        vSurfaceJson = json.load(f)
+    
+    for vSurfaceJsonData in vSurfaceJson:
+        
+        # create aSurfaceData dataset
+        aSurfaceData = vImarisApplication.GetFactory().CreateDataSet()
+        vData = np.array(vSurfaceJsonData['mask'], dtype=np.uint16)
+        vSizeX, vSizeY, vSizeZ = np.shape(vData)
+        aSurfaceData.Create(Imaris.tType.eTypeUInt16, vSizeX, vSizeY, vSizeZ, 1, 1) 
+        aSurfaceData.SetDataVolumeFloats(vData.tolist(), aIndexC = 0, aIndexT = 0)
+
+        aSurfaceData.SetExtendMinX(vSurfaceJsonData['xRange'][0])
+        aSurfaceData.SetExtendMaxX(vSurfaceJsonData['xRange'][1])
+
+        aSurfaceData.SetExtendMinY(vSurfaceJsonData['yRange'][0])
+        aSurfaceData.SetExtendMaxY(vSurfaceJsonData['yRange'][1])
+
+        aSurfaceData.SetExtendMinZ(vSurfaceJsonData['zRange'][0])
+        aSurfaceData.SetExtendMaxZ(vSurfaceJsonData['zRange'][1])
+
+        # add aSurfaceData to Surfaces
+        vSurfaces.AddSurface(aSurfaceData, 0) # second number is time index which is irrelevant
+
+    vSurfaces.SetName('Imported Surfaces')
+
+    # add to scene
+    vScene = vImarisApplication.GetSurpassScene()
+    vScene.AddChild(vSurfaces, -1)
+
+    logging.info(
+        f'Imported %d surfaces',
+        len(vSurfaceJson),
+    )
+    logging.info('----- Done importing surfaces -----')
+
+def ImportSurfaces(aImarisId):
+    # Create an ImarisLib object
+    vImarisLib = ImarisLib.ImarisLib()
+
+    # Get an imaris object with id aImarisId
+    vImarisApplication = vImarisLib.GetApplication(aImarisId)
+
+    # Initialize and launch Tk window, then hide it.
+    vRootTkWindow = Tk()
+    vRootTkWindow.withdraw()
+
+    # Check if the object is valid
+    if vImarisApplication is None:
+        messagebox.showerror('Error', f'Failed to connect to Imaris application (id={aImarisId})')
+        return
+
+    print(f'Connected to Imaris application (id={aImarisId})')
+
+    try:
+        Main(vImarisApplication)
+    except Exception as exception:
+        print(traceback.print_exception(type(exception), exception, exception.__traceback__))
+    messagebox.showinfo('Complete', 'The XTension has terminated.')
